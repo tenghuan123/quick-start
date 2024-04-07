95da8d298adbb007e2f8edc6d6cf23aaeb97322f
玄星 <2540149177@qq.com>
2024-04-07 20:34:59 +0800

    fix: Integrate the toast component into the storybook

--- packages/ui/packages/components/toast/stories/toast.tsx
+++ packages/ui/packages/components/toast/stories/toast.tsx
@@ -1,26 +1,38 @@
-"use client"
+'use client'
 
-import { Button } from "../../button"
-import { ToastAction } from "../toast"
-import { useToast } from "../use-toast"
+import { Toaster } from '../index'
+import { Button } from '../../button'
+import { ToastAction } from '../toast'
+import { useToast } from '../use-toast'
+
+export function ToastProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      {children}
+      <Toaster />
+    </>
+  )
+}
 
 export function ToastDemo() {
   const { toast } = useToast()
 
   return (
-    <Button
-      variant="outline"
-      onClick={() => {
-        toast({
-          title: "Scheduled: Catch up ",
-          description: "Friday, February 10, 2023 at 5:57 PM",
-          action: (
-            <ToastAction altText="Goto schedule to undo">Undo</ToastAction>
-          ),
-        })
-      }}
-    >
-      Add to calendar
-    </Button>
+    <ToastProvider>
+      <Button
+        variant="outline"
+        onClick={() => {
+          toast({
+            title: 'Scheduled: Catch up ',
+            description: 'Friday, February 10, 2023 at 5:57 PM',
+            action: (
+              <ToastAction altText="Goto schedule to undo">Undo</ToastAction>
+            ),
+          })
+        }}
+      >
+        Add to calendar
+      </Button>
+    </ToastProvider>
   )
 }
